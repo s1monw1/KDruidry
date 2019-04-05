b01f648567c7115d984e00de545b67335120c0b0
Simon Wirtz <swirtz@exactag.com>
2019-04-05 15:19:39 +0200

    import simplification

--- src/main/kotlin/de/swirtz/ktsrunner/kdruidry/druidextensions.kt
+++ src/main/kotlin/de/swirtz/ktsrunner/kdruidry/druidextensions.kt
@@ -2,8 +2,10 @@ package de.swirtz.ktsrunner.kdruidry
 
 import `in`.zapr.druid.druidry.Interval
 import java.time.ZonedDateTime
+import org.joda.time.DateTime as JodaDateTime
 
 fun intervalBetween(from: ZonedDateTime, to: ZonedDateTime) =
     Interval(from.toLocalDateJodaTime(), to.toLocalDateJodaTime())
 
-fun ZonedDateTime.toLocalDateJodaTime() = org.joda.time.DateTime(year, monthValue, dayOfMonth, hour, minute, second)
+fun ZonedDateTime.toLocalDateJodaTime() =
+    JodaDateTime(year, monthValue, dayOfMonth, hour, minute, second)
